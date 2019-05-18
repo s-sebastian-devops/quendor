8621c0d6e7cf1054b037249877fe0dbf3ca2adbc
Jeff Nyman <jeffnyman@gmail.com>
2019-05-18 08:35:54 -0500

    Start of option processing logic; mainly dealing with presentation.

--- quendor/__main__.py
+++ quendor/__main__.py
@@ -1,12 +1,37 @@
 import sys
+import argparse
+import textwrap
 
 if sys.version_info < (3, 0):
     sys.stderr.write("Quendor requires Python 3.\n")
     sys.exit(1)
 
 
+def process_options(opts):
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        description="Options:",
+        usage=textwrap.dedent(
+            """
+            The general format is:
+
+                python3 -m quendor
+            """
+        ),
+        epilog=textwrap.dedent(
+            """
+            Enjoy your visit to Quendor!
+            """
+        ),
+    )
+
+    parser.parse_args(opts)
+
+
 def main():
-    print("Welcome to Quendor!")
+    print("Quendor Z-Machine Interpreter\n")
+
+    process_options(sys.argv[1:])
 
 
 if __name__ == "__main__":
