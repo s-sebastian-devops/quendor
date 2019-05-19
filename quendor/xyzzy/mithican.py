2b42c0e4319748abded1af8cfa35194120031a3e
Jeff Nyman <jeffnyman@gmail.com>
2019-05-19 08:45:54 -0500

    (Mithican) Story data is provided as a bytearray that provides the basis for a memory map.

--- quendor/xyzzy/mithican.py
+++ quendor/xyzzy/mithican.py
@@ -1,2 +1,9 @@
+class Memory:
+    def __init__(self, memory):
+        self.memory = bytearray(memory)
+
+
 def execute(story_data):
     print("\nA hollow voice says 'As you wish...'\n")
+
+    Memory(story_data)
